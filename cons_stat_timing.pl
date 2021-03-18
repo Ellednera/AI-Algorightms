6249fc0f110eb609f6101deb37c1bdd2236cdf0e
Raphael Jun Jie Jong <73269803+Ellednera@users.noreply.github.com>
2021-03-18 13:46:56 +0800

    Update cons_stat_timing.pl

--- cons_stat_timing.pl
+++ cons_stat_timing.pl
@@ -52,31 +52,23 @@ timethese(0, {
 				}
 			},
 	loops => sub {
-				# my @row_a = (1, 2, 6, 33, 10);
-				# my @row_b = (45, 31, 23);
-				# my @row_c = (1, 2, 7, 5);
-				A:for my $A (@row_a) {
-					B:for my $B (@row_b) {
-						next B if $A <= $B;
-						C:for my $C (@row_c) {
-							# say "($A,$B,$C)";
-							if ( ($B != $C) and ($A != $C) ) {
-								# say "Solution found: ($A,$B,$C)";
-								last A;
-							}
+			# my @row_a = (1, 2, 6, 33, 10);
+			# my @row_b = (45, 31, 23);
+			# my @row_c = (1, 2, 7, 5);
+			A:for my $A (@row_a) {
+				B:for my $B (@row_b) {
+					next B if $A <= $B;
+					C:for my $C (@row_c) {
+						# say "($A,$B,$C)";
+						if ( ($B != $C) and ($A != $C) ) {
+							# say "Solution found: ($A,$B,$C)";
+							last A;
 						}
 					}
 				}
-			},
+			}
+		},
 });
-
-# When considering the module (don't do that, the while loop is faster)
-# We are interested in the checking part, not the module part
-# Benchmark: running cross_product, loops for at least 3 CPU seconds...
-# cross_product:  3 wallclock secs ( 3.14 usr +  0.00 sys =  3.14 CPU) @ 415.31/s(n=1302)
-#         loops:  3 wallclock secs ( 3.12 usr +  0.00 sys =  3.12 CPU) @ 108876.56/s(n=339586)
-
-
 
 # besiyata d'shmaya
 
